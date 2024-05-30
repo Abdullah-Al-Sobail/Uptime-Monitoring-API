9db774c4642c3ec7c79d94a8962e28802c9ed775
Abdullah Al Sobail <abdullahalsobail78@gmail.com>
2024-05-30 09:39:37 +0600

    Updated

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 */
 //Dependencies
 const http=require('http');
+const url=require('url');
 
 //app object - module scaffolding
 const app={};
@@ -25,6 +26,15 @@ app.createserver = () => {
 }
 //handale Request Response 
 app.handleReqRes=(req,res)=>{
+    //req handling
+    //get the url and parse it
+    const parsedUrl=url.parse(req.url,true);
+    const path=parsedUrl.pathname;
+    const trimmedPath=path.replace(/^\/+|\/+$/g,'');
+    const method=req.method.toLowercase();
+    const querryStringObject=parsedUrl.query;
+    const headersObject=req.headers;
+     
     //response handle
     res.end(' World');
 }
